7d2c597879826908a61795bd71bd39ff24eac23a
Navneet Singh <144220801+Navi2510@users.noreply.github.com>
2024-03-23 18:59:04 +0000

    Conversion Operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = 33
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let score1="33abc"
+console.log(typeof score1);
+
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+/*
+
+"33" => 33
+"33abc" => NaN
+true =>1; false => 0
+
+
+*/
