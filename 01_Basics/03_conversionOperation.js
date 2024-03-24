4f5adb33b2f7c02519b52d976f1bec0df27b4440
Navneet Singh <144220801+Navi2510@users.noreply.github.com>
2024-03-24 05:16:42 +0000

    summary

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,14 +1,14 @@
 let score = 33
 
-console.log(typeof score)
-console.log(typeof(score));
+//console.log(typeof score)
+//console.log(typeof(score));
 
 let score1="33abc"
-console.log(typeof score1);
+//console.log(typeof score1);
 
 let valueInNumber = Number(score1)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
 /*
@@ -19,3 +19,47 @@ true =>1; false => 0
 
 
 */
+
+isLoggedIn = "Hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=> true; 0=>false
+// " " =>false
+//"hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//********************************Operations******************************** */
+
+let value=3
+let negValue = -value
+// console.log(negValue)
+
+let str1= "Hello"
+let str2 = " Hitesh"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1+ "2");
+// console.log("1" + 2 + "2");
+// console.log(1+2+"2");
+
+//console.log(+true)
+//console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3 = 2+2
+
+let gameCounter =100
+++gameCounter
+console.log(gameCounter);
---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0);
+// console.log(undefined>0)
+// console.log(undefined<0);
+
+// ye uper wale comparison nhi krne chaiye ye best practice nhi hai
+
+// ===
+
+console.log("2"== 2)
+console.log("2" === 2);
