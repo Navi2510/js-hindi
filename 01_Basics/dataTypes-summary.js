c52f08f8592be8153434b52c496fa9cab7d09c8d
Navneet Singh <144220801+Navi2510@users.noreply.github.com>
2024-03-24 06:19:38 +0000

    summary

--- 01_Basics/dataTypes-summary.js
+++ 01_Basics/dataTypes-summary.js
@@ -30,8 +30,42 @@ let  myObj ={
 }
 
 const myFunction = function(){
-    console.log("Hello World")
+   // console.log("Hello World")
 }
 
-console.log( typeof bigNumber)
-console.log(typeof outsideTemp)
+//console.log( typeof bigNumber)
+//console.log(typeof outsideTemp)
+
+
+//************************************************************** */
+
+// Stack (Primitive) , Heap (Non - Primitive)
+
+let myYoutubename = "NavneetSinghdotcom"
+
+let anotherName =  myYoutubename
+
+anotherName = "chaiaurcode"
+
+// console.log(myYoutubename)
+// console.log(anotherName);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+
+let userTwo = userOne;
+
+userTwo.email ="navneetsinghit25@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+// jitne bhi hmare primitive values hai ye jate hai stack k andar aur stack k andar jo bhi cheez lenge ti uska ek copy hi milta hai
+
+// aur non - prmitives data types -- heap me jate hai aur heap me jo bhi rakhte hai to uska reference milta hai --- mtlb jo bhi value update krte hai wo original value me changes hota hai
+
+
